3e85e5bc6e917fa52aa5097181a9a40cd9fce421
ChaoticSomeone256 <ddemiral@htl-steyr.ac.at>
2025-04-26 14:26:03 +0200

    Small fix

--- src/main/java/com/chaoticsomeone/ModInfGen/ModuleInfoGenerator.java
+++ src/main/java/com/chaoticsomeone/ModInfGen/ModuleInfoGenerator.java
@@ -173,7 +173,7 @@ public class ModuleInfoGenerator {
 		return collapseWhitespaces;
 	}
 
-	public void setCollapseWhitespaces(boolean collapseWhitespaces) {
+	private void setCollapseWhitespaces(boolean collapseWhitespaces) {
 		this.collapseWhitespaces = collapseWhitespaces;
 	}
 
